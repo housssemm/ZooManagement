75d3a21a0dc4692bfb38a0888fcf93f6189d3541
houssem <95879355+housssemm@users.noreply.github.com>
2024-09-16 20:39:43 +0100

    first commit

---
+++ src/ZooManagement.java
@@ -0,0 +1,11 @@
+public class ZooManagement {
+        public static void main(String[]args){
+
+            Animal lion = new Animal();
+            Zoo myZoo = new Zoo("zoo" ,"tunis",20);
+            myZoo.displayZoo();
+            System.out.println(myZoo);
+            System.out.println(myZoo.toString());
+        }
+
+}
